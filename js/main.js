8eb9babea209b3be1234ff8e3adb1513cd79ab54
Ross Thorn <rthorn@wisc.edu>
2017-04-18 19:24:07 -0500

    Initial Commit
    Map along with unpopulated geojson.

---
+++ js/main.js
@@ -0,0 +1,116 @@
+(function(){
+
+var attributes = [];
+
+function createMap(){
+
+    // var southWest = L.latLng(39, -98),
+    // northEast = L.latLng(50, -79),
+    // bounds = L.latLngBounds(southWest, northEast);
+
+    //create the map
+    var map = L.map('mapid', {
+        center: [39, -98],
+        zoom: 4,
+        // maxBounds: bounds,
+        maxBoundsViscosity:.7,
+        minZoom: 4
+    });
+
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+      	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      	subdomains: 'abcd',
+        minZoom:2
+    }).addTo(map);
+
+    // Create necessary panes in correct order
+    // map.createPane("pointsPane");
+    // map.createPane("polygonsPane");
+
+
+    //call getData function
+        getCountryShapeData(map);
+//         getData(map);
+        $(window).on("resize", function () { $("#mapid").height($(window).height()); map.invalidateSize(); }).trigger("resize");
+        $(document).ready(function() {$(window).resize(function() {
+        var bodyheight = $(this).height();
+        $("#page-content").height(bodyheight-70);
+          }).resize();
+        });
+
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+function getCountryShapeData(map){
+    //load the data
+    $.ajax("data/States.geojson", {
+        dataType: "json",
+        success: function(response){
+          var polyAttributes = processPolyData(response);
+            createPolygons(response, map, attributes);
+        }
+    });
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+function processPolyData(data){
+  //properties of the first feature in the dataset
+  var properties = data.features[0].properties;
+  //push each attribute name into attributes array
+  for (var attribute in properties){
+      //only take attributes with Rank values
+      if (attribute.indexOf("postal") > -1){
+          attributes.push(attribute);
+          console.log(properties[attribute]);
+      };
+  };
+
+
+  return attributes;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+function createPolygons(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    console.log(data);
+    console.log(map);
+    console.log(attributes);
+    var polyLayer = L.geoJson(data, {
+      style: function(feature){
+        //console.log(feature);
+        var options = {
+            fillColor: "tomato",
+            weight: .5,
+            color: "chartreuse",
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        //console.log(options.fillColor);
+        return options;
+      },
+      // pane:"polygonsPane"
+    });
+   console.log(polyLayer);
+   var overlays = {
+    "Countries with<br> States": polyLayer
+};
+     L.control.layers(null,overlays,{collapsed:false}).addTo(map);
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+$(document).ready(createMap);
+
+})();
