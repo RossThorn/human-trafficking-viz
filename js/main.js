cfaaf2a1ea53d4db1f7aa6781a2fe05af53317ea
LeanneAbraham <abraham.leanne@gmail.com>
2017-05-02 18:38:14 -0500

    Fixed the code for readability
    Made some global variables, etc. Should be easier to work with moving
    forward.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 (function(){
 
-  var attributes = [];
-
-
-
-  // var southWest = L.latLng(39, -98),
-  // northEast = L.latLng(50, -79),
-  // bounds = L.latLngBounds(southWest, northEast);
-
-  //create the map
   var map = L.map('big-map-canvas', {
     center: [40, -125],
     zoom: 4,
@@ -18,7 +9,6 @@
     scrollWheelZoom: false
   });
 
-
   //add OSM base tilelayer
   L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
@@ -26,91 +16,63 @@
     minZoom:2
   }).addTo(map);
 
-  // Create necessary panes in correct order
-  // map.createPane("pointsPane");
-  // map.createPane("polygonsPane");
+  //code watches for when the user scrolls to teh first section1
+  var firstSectionWatcher = scrollMonitor.create($('#section1'));
+  //return statement notifying when this happens
+  firstSectionWatcher.enterViewport(function () {
+    console.log('in viewport now');
+    var firstchange = map.setView(new L.LatLng(46,-94), 6, {animate: true});
+  });
 
+  $(window).on("resize", function () {
+    $("#big-map-canvas").height($(window).height());
+    map.invalidateSize();
+  }).trigger("resize");
 
-  //call getData function
-  getCountryShapeData(map);
-  //         getData(map);
-  $(window).on("resize", function () { $("#big-map-canvas").height($(window).height()); map.invalidateSize(); }).trigger("resize");
-  $(document).ready(function() {$(window).resize(function() {
-    var bodyheight = $(this).height();
-    $("#page-content").height(bodyheight-70);
-  }).resize();
-});
+  $(document).ready(function() {
+    $(window).resize(function() {
+      var bodyheight = $(this).height();
+      $("#page-content").height(bodyheight-70);
+    }).resize();
+  });
 
-
-////////////////////////////////////////////////////////////////////////////////
-
-function getCountryShapeData(map){
-  //load the data
   $.ajax("data/States.geojson", {
     dataType: "json",
-    success: function(response){
-      var polyAttributes = processPolyData(response);
-      createPolygons(response, map, attributes);
-    }
+    success: createPolygons
   });
-};
 
-////////////////////////////////////////////////////////////////////////////////
-
-function processPolyData(data){
-  //properties of the first feature in the dataset
-  var properties = data.features[0].properties;
-  //push each attribute name into attributes array
-  for (var attribute in properties){
-    //only take attributes with Rank values
-    if (attribute.indexOf("postal") > -1){
-      attributes.push(attribute);
-    };
-  };
-
-
-  return attributes;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-
-function createPolygons(data, map, attributes){
-  //create a Leaflet GeoJSON layer and add it to the map
-  var polyLayer = L.geoJson(data, {
-    style: function(feature){
-      //console.log(feature);
-      var options = {
-        fillColor: "tomato",
-        weight: .5,
-        color: "chartreuse",
-        opacity: 1,
-        fillOpacity: 0.8
+  function processPolyData(data){
+    var attributes = [];
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+      //only take attributes with Rank values
+      if (attribute.indexOf("postal") > -1){
+        attributes.push(attribute);
       };
-
-      //console.log(options.fillColor);
-      return options;
-    },
-    // pane:"polygonsPane"
-  });
-  var overlays = {
-    "States": polyLayer
+    };
+    return attributes;
   };
-  L.control.layers(null,overlays,{collapsed:false}).addTo(map);
 
-};
-
-//code watches for when the user scrolls to teh first section1
-var firstSectionWatcher = scrollMonitor.create($('#section1'));
-//return statement notifying when this happens
-firstSectionWatcher.enterViewport(function () {
-  console.log('in viewport now');
-  var firstchange = map.setView(new L.LatLng(46,-94), 6, {animate: true});
-}
-);
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-//$(document).ready(createMap);
-
+  function createPolygons(data) {
+    var attributes = processPolyData(data);
+    //create a Leaflet GeoJSON layer and add it to the map
+    var polyLayer = L.geoJson(data, {
+      style: function(feature){
+        var options = {
+          fillColor: "tomato",
+          weight: .5,
+          color: "chartreuse",
+          opacity: 1,
+          fillOpacity: 0.8
+        };
+        return options;
+      },
+    });
+    var overlays = {
+      "States": polyLayer
+    };
+    L.control.layers(null,overlays,{collapsed:false}).addTo(map);
+  };
 })();
